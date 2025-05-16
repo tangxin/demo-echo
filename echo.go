f66c4c6534be8f48a1c70fd71ad0488c2522bf9d
tangx <uyinn28@gmail.com>
2025-05-16 11:30:21 +0800

    feat: new sub and divide function

--- echo.go
+++ echo.go
@@ -13,3 +13,14 @@ func Add(a, b int) int {
 func Multiply(a, b int) int {
 	return a * b
 }
+
+func Subtract(a, b int) int {
+	return a - b
+}
+
+func Divide(a, b int) (int, error) {
+	if b == 0 {
+		return 0, fmt.Errorf("cannot divide by zero")
+	}
+	return a / b, nil
+}
